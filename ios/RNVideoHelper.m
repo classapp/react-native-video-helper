0d4958bc6ac6c1a0cf4bb75be6334f9203067817
Samin Shams <saminshams@gmail.com>
2018-10-17 15:20:14 -0300

    added progress in NSLog

--- ios/RNVideoHelper.m
+++ ios/RNVideoHelper.m
@@ -15,48 +15,60 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(compress:(NSString *)source options:(NSDictionary *)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
     
     NSURL *url = [[NSURL alloc] initWithString:source];
-
+    
     AVURLAsset *asset = [AVURLAsset URLAssetWithURL:url options:nil];
-
+    
     SDAVAssetExportSession *encoder = [SDAVAssetExportSession.alloc initWithAsset:asset];
     encoder.outputFileType = AVFileTypeMPEG4;
     encoder.outputURL = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:[NSString stringWithFormat:@"lowerBitRate-%d.mov",arc4random() % 1000]]];
     
     encoder.videoSettings = @{
-        AVVideoCodecKey: AVVideoCodecH264,
-        AVVideoWidthKey: options[@"width"],
-        AVVideoHeightKey: options[@"height"],
-        AVVideoCompressionPropertiesKey: @{
-            AVVideoAverageBitRateKey: options[@"bitrate"],
-            AVVideoProfileLevelKey: AVVideoProfileLevelH264High40,
-        },
-    };
-    encoder.audioSettings = @{
-        AVFormatIDKey: @(kAudioFormatMPEG4AAC),
-        AVNumberOfChannelsKey: @2,
-        AVSampleRateKey: @44100,
-        AVEncoderBitRateKey: @96000,
-    };
+                              AVVideoCodecKey: AVVideoCodecH264,
+                              AVVideoWidthKey: options[@"width"],
+                              AVVideoHeightKey: options[@"height"],
+                              AVVideoCompressionPropertiesKey: @{
+                                      AVVideoAverageBitRateKey: options[@"bitrate"],
+                                      AVVideoProfileLevelKey: AVVideoProfileLevelH264High40,
+                                      },
+                              };
+    
+    if (!options[@"removeAudio"]) {
+        encoder.audioSettings = @{
+                                  AVFormatIDKey: @(kAudioFormatMPEG4AAC),
+                                  AVNumberOfChannelsKey: @2,
+                                  AVSampleRateKey: @44100,
+                                  AVEncoderBitRateKey: @96000,
+                                  };
+    }
+    
 
-    [encoder exportAsynchronouslyWithCompletionHandler:^
-    {
-        if (encoder.status == AVAssetExportSessionStatusCompleted)
-        {
-            resolve(encoder.outputURL.absoluteString);
-            NSLog(@"Video export succeeded");
-        }
-        else if (encoder.status == AVAssetExportSessionStatusCancelled)
-        {
-            NSLog(@"Video export cancelled");
-            reject(@"Video export cancelled", nil, nil);
-        }
-        else
-        {
-            NSLog(@"Video export failed with error: %@ (%ld)", encoder.error.localizedDescription, encoder.error.code);
-            reject(@"Video export failed", nil, nil);
-        }
+    __block NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:2 repeats:YES block:^(NSTimer * _Nonnull timer) {
+        NSLog(@"Helllo wotld %2f", encoder.progress);
     }];
+    
+    [encoder exportAsynchronouslyWithCompletionHandler:^
+     {
+         [timer invalidate];
+         timer = nil;
+
+         if (encoder.status == AVAssetExportSessionStatusCompleted)
+         {
+             resolve(encoder.outputURL.absoluteString);
+             NSLog(@"Video export succeeded");
+         }
+         else if (encoder.status == AVAssetExportSessionStatusCancelled)
+         {
+             NSLog(@"Video export cancelled");
+             reject(@"Video export cancelled", nil, nil);
+         }
+         else
+         {
+             NSLog(@"Video export failed with error: %@ (%ld)", encoder.error.localizedDescription, encoder.error.code);
+             reject(@"Video export failed", nil, nil);
+         }
+     }];
 }
+
 
 RCT_EXPORT_METHOD(trim:(NSString *)source options:(NSDictionary *)options resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
     resolve(source);
